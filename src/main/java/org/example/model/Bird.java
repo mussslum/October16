0922f5b27edb2f625e85b37724950a4555a561aa
Muslum Misirli <146063342+mussslum@users.noreply.github.com>
2024-12-02 01:45:42 +0400

    Update Bird.java

--- src/main/java/org/example/model/Bird.java
+++ src/main/java/org/example/model/Bird.java
@@ -3,7 +3,6 @@ package org.example.model;
 public class Bird {
     public void eat(){
         System.out.println("Bird can eat");
-        // sdadmkda cac
     }
     public void fly(){
         System.out.println("Bird can fly");
